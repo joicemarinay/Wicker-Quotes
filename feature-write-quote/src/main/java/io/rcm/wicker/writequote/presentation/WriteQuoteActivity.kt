b93c16d80f9d90273cd79456b6fc61c9aad45bf4
Joice Marinay <joicemarinay@gmail.com>
2018-05-16 20:24:35 +0800

    Create Activity for Write Quote module

---
+++ feature-write-quote/src/main/java/io/rcm/wicker/writequote/presentation/WriteQuoteActivity.kt
@@ -0,0 +1,17 @@
+package io.rcm.wicker.writequote.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.rcm.wicker.writequote.R
+
+/**
+ * Created by joicemarinay on 09/05/2018.
+ */
+internal class WriteQuoteActivity: AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.wicker_write_quote_view)
+  }
+
+}
