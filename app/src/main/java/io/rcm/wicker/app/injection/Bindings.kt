52b589821f76382c412ff1df62cbcddf52f3dc1b
Joice Marinay <joicemarinay@gmail.com>
2018-05-16 14:30:34 +0800

    Implement opening of WriteQuote screen from QuoteList screen

---
+++ app/src/main/java/io/rcm/wicker/app/injection/Bindings.kt
@@ -0,0 +1,22 @@
+package io.rcm.wicker.app.injection
+
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+import io.rcm.wicker.app.injection.scopes.PerActivity
+import io.rcm.wicker.quotelist.presentation.QuoteListActivity
+import io.rcm.wicker.writequote.presentation.WriteQuoteActivity
+
+/**
+ * Created by joicemarinay on 16/05/2018.
+ */
+@Module
+internal abstract class Bindings {
+
+  @PerActivity
+  @ContributesAndroidInjector
+  abstract fun bindQuoteListActivity(): QuoteListActivity
+
+  @PerActivity
+  @ContributesAndroidInjector
+  abstract fun bindWriteQuoteActivity(): WriteQuoteActivity
+}
