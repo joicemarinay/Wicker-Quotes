887b21b3aa2c3a449825261be686f7ba22320429
Joice Marinay <joicemarinay@gmail.com>
2018-05-17 11:44:09 +0800

    Make WriteQuoteActivity extend BaseActivity

--- feature-write-quote/src/main/java/io/rcm/wicker/writequote/presentation/WriteQuoteActivity.kt
+++ feature-write-quote/src/main/java/io/rcm/wicker/writequote/presentation/WriteQuoteActivity.kt
@@ -1,7 +1,6 @@
 package io.rcm.wicker.writequote.presentation
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import io.rcm.wicker.base.presentation.BaseActivity
 import io.rcm.wicker.writequote.R
 
 /**
@@ -10,11 +9,5 @@ import io.rcm.wicker.writequote.R
  * This Activity is not internal because it is used by
  * [io.rcm.wicker.app.view.ScreenRouterImpl]
  */
-class WriteQuoteActivity: AppCompatActivity() {
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.wicker_write_quote_view)
-  }
-
-}
+class WriteQuoteActivity(override val layoutResourceId: Int = R.layout.wicker_write_quote_view):
+    BaseActivity()
