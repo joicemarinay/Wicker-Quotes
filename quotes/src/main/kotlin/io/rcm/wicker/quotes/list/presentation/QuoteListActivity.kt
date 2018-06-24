1e90a2d3026f5924b9a74783bd41fefd45d06c55
Joice Marinay <joicemarinay@gmail.com>
2018-06-24 13:08:57 +0800

    Directly open WriteQuote from QuoteList

--- quotes/src/main/kotlin/io/rcm/wicker/quotes/list/presentation/QuoteListActivity.kt
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/list/presentation/QuoteListActivity.kt
@@ -1,10 +1,12 @@
 package io.rcm.wicker.quotes.list.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import io.rcm.wicker.base.presentation.BaseActivity
 import io.rcm.wicker.base.presentation.ScreenRouter
 import io.rcm.wicker.base.presentation.ScreenRouter.Screen
 import io.rcm.wicker.quotes.R
+import io.rcm.wicker.quotes.write.presentation.WriteQuoteActivity
 import kotlinx.android.synthetic.main.wicker_quote_list_view.*
 import javax.inject.Inject
 
@@ -26,8 +28,8 @@ class QuoteListActivity(override val layoutResourceId: Int = R.layout.wicker_quo
     quoteList_fab_addQuote.setOnClickListener { openWriteQuote() }
   }
 
-  //TODO add [QuoteEntity] as param
+  //TODO add [QuoteEntity] as param (when editing a quote)
   private fun openWriteQuote() {
-    startActivity(screenRouter.getScreenIntent(this, Screen.Write))
+    startActivity(Intent(this, WriteQuoteActivity::class.java))
   }
 }
