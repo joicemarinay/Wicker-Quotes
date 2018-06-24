aceb074828fff0f4c55f6863bae56c577a7ab4c7
Joice Marinay <joicemarinay@gmail.com>
2018-06-24 17:03:52 +0800

    Create quotes database

---
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/data/local/db/QuoteInDb.kt
@@ -0,0 +1,48 @@
+package io.rcm.wicker.quotes.data.local.db
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ *
+ * Model used solely for transacting quotes in and out of DB
+ *
+ * Created by joicemarinay on 18/04/2018.
+ */
+@Entity(tableName = QuoteInDb.TABLE_NAME)
+internal data class QuoteInDb(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = COLUMN_ID)
+    val id: Int,
+
+    @ColumnInfo(name = COLUMN_QUOTE)
+    val quote: String,
+
+    @ColumnInfo(name = COLUMN_AUTHOR)
+    val author: String,
+
+    @ColumnInfo(name = COLUMN_SOURCE_NAME)
+    val sourceName: String,
+
+    @ColumnInfo(name = COLUMN_SOURCE_URL)
+    val sourceUrl: String,
+
+    @ColumnInfo(name = COLUMN_IS_FAVOURITE)
+    val isFavourite: Boolean,
+
+    @ColumnInfo(name = COLUMN_IS_DELETED)
+    val isDeleted: Boolean
+//TODO add tags
+) {
+  companion object {
+    const val TABLE_NAME = "quotes"
+    const val COLUMN_ID = "quoteId"
+    const val COLUMN_QUOTE = "quote"
+    const val COLUMN_AUTHOR = "author"
+    const val COLUMN_SOURCE_NAME = "sourceName"
+    const val COLUMN_SOURCE_URL = "sourceUrl"
+    const val COLUMN_IS_FAVOURITE = "isFavourite"
+    const val COLUMN_IS_DELETED = "isDeleted"
+  }
+}
---
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/data/local/db/QuotesDb.kt
@@ -0,0 +1,39 @@
+package io.rcm.wicker.quotes.data.local.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import javax.inject.Singleton
+
+/**
+ * Created by joicemarinay on 6/24/18.
+ */
+@Database(version = 1, exportSchema = false,
+    entities = [
+      QuoteInDb::class
+    ]
+)
+@Singleton
+internal abstract class QuotesDb: RoomDatabase() {
+
+  internal abstract fun quotesDao(): QuotesDao
+
+  companion object {
+    @Volatile
+    private var INSTANCE: QuotesDb? = null
+
+    private const val DB_NAME = "wicker-rcm-io.db"
+
+    fun getInstance(context: Context): QuotesDb =
+        INSTANCE ?: synchronized(this) {
+          INSTANCE
+              ?: buildDatabase(context)
+              .also { INSTANCE = it }
+        }
+
+    private fun buildDatabase(context: Context) =
+        Room.databaseBuilder(context.applicationContext, QuotesDb::class.java, DB_NAME).build()
+  }
+
+}
