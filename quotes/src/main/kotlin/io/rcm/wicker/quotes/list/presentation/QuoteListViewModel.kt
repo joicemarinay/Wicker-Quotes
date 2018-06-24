d97a93a186a02a90847a56c05db3deaa41e0e8cf
Joice Marinay <joicemarinay@gmail.com>
2018-06-24 18:10:14 +0800

    Setup ViewModel classes for quotes

---
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/list/presentation/QuoteListViewModel.kt
@@ -0,0 +1,9 @@
+package io.rcm.wicker.quotes.list.presentation
+
+import io.rcm.wicker.base.presentation.BaseViewModel
+import javax.inject.Inject
+
+/**
+ * Created by joicemarinay on 6/24/18.
+ */
+internal class QuoteListViewModel @Inject constructor(): BaseViewModel()
---
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterViewModel.kt
@@ -0,0 +1,9 @@
+package io.rcm.wicker.quotes.writer.presentation
+
+import io.rcm.wicker.base.presentation.BaseViewModel
+import javax.inject.Inject
+
+/**
+ * Created by joicemarinay on 6/24/18.
+ */
+internal class QuoteWriterViewModel @Inject constructor(): BaseViewModel()
