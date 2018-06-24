51648274def173d12fad17343e7c03e7b12b2197
Joice Marinay <joicemarinay@gmail.com>
2018-06-24 18:10:14 +0800

    Setup ViewModel classes for quotes

--- base/src/main/kotlin/io/rcm/wicker/base/presentation/BaseActivity.kt
+++ base/src/main/kotlin/io/rcm/wicker/base/presentation/BaseActivity.kt
@@ -1,17 +1,27 @@
 package io.rcm.wicker.base.presentation
 
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
+import javax.inject.Inject
 
 /**
  * This should be implemented by all Activities instead of [AppCompatActivity].
  * Prevents repetition of certain actions such as setting of layout resource
  */
-abstract class BaseActivity: AppCompatActivity() {
+abstract class BaseActivity<T: ViewModel>: AppCompatActivity() {
+
+  @Inject
+  protected lateinit var viewModel: T
+
+  @Inject
+  protected lateinit var viewModelFactory: ViewModelFactory
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    viewModel = ViewModelProviders.of(this, viewModelFactory).get(viewModel.javaClass)
     setContentView(layoutResourceId)
   }
 
