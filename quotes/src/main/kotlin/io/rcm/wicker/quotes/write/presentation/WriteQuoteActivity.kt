e84cd1539ffcde4a3cae970ac68dd0b296ddd64a
Joice Marinay <joicemarinay@gmail.com>
2018-06-24 12:24:24 +0800

    Create unified module for quote list and write quote
    And eventually, quote detail

---
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/write/presentation/WriteQuoteActivity.kt
@@ -0,0 +1,13 @@
+package io.rcm.wicker.quotes.write.presentation
+
+import io.rcm.wicker.base.presentation.BaseActivity
+import io.rcm.wicker.quotes.R
+
+/**
+ * Created by joicemarinay on 09/05/2018.
+ *
+ * This Activity is not internal because it is used by
+ * [io.rcm.wicker.app.view.ScreenRouterImpl]
+ */
+class WriteQuoteActivity(override val layoutResourceId: Int = R.layout.wicker_write_quote_view):
+    BaseActivity()
