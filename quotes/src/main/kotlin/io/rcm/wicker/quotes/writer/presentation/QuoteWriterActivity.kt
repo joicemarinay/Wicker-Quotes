681c7cd191674b7f711db56145782e48b1d6af6b
Joice Marinay <joicemarinay@gmail.com>
2018-06-27 05:49:55 +0800

    Implement saving of quote in presentation layer

--- quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterActivity.kt
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterActivity.kt
@@ -1,10 +1,13 @@
 package io.rcm.wicker.quotes.writer.presentation
 
 import android.os.Bundle
+import io.rcm.wicker.base.common.observe
 import io.rcm.wicker.base.presentation.BaseActivity
 import io.rcm.wicker.quotes.QuotesDependencyHolder
 import io.rcm.wicker.quotes.R
 import io.rcm.wicker.quotes.writer.injection.QuoteWriterComponent
+import io.rcm.wicker.quotes.writer.presentation.QuoteWriterViewModel.State
+import kotlinx.android.synthetic.main.wicker_quote_writer_view.*
 
 /**
  * Created by joicemarinay on 09/05/2018.
@@ -20,5 +23,38 @@ internal class QuoteWriterActivity(override val layoutResourceId: Int = R.layout
   override fun onCreate(savedInstanceState: Bundle?) {
     component.inject(this)
     super.onCreate(savedInstanceState)
+    setClickListeners()
+    setDataObservers()
+  }
+
+  private fun inputAuthor(): String = quoteWriter_editText_author.text.toString()
+
+  private fun inputLink(): String = quoteWriter_editText_link.text.toString()
+
+  private fun inputQuote(): String = quoteWriter_editText_quote.text.toString()
+
+  private fun inputSource(): String = quoteWriter_editText_source.text.toString()
+
+  private fun onStateChange(state: State) = when(state) {
+    is State.Loading -> showLoading()
+    State.SaveFailed -> showError()
+    State.SaveOk -> finish()
+  }
+
+  private fun setClickListeners() {
+    quoteWriter_button_save.setOnClickListener { viewModel.onClickSave(quote = inputQuote(),
+      author = inputAuthor(), sourceName = inputSource(), sourceUrl = inputLink()) }
+  }
+
+  private fun setDataObservers() {
+    observe(viewModel.state()) { onStateChange(it) }
+  }
+
+  private fun showError() {
+    //TODO
+  }
+
+  private fun showLoading() {
+    //TODO display loading
   }
 }
--- quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterViewModel.kt
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterViewModel.kt
@@ -1,17 +1,53 @@
 package io.rcm.wicker.quotes.writer.presentation
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
 import io.rcm.wicker.base.presentation.BaseViewModel
 import io.rcm.wicker.quotes.QuotesDependencyHolder
+import io.rcm.wicker.quotes.domain.model.QuoteEntity
+import io.rcm.wicker.quotes.writer.domain.SaveQuote
 import javax.inject.Inject
 
 /**
  * Created by joicemarinay on 6/24/18.
  */
-internal class QuoteWriterViewModel @Inject constructor(): BaseViewModel() {
+internal class QuoteWriterViewModel @Inject constructor(private val saveQuote: SaveQuote):
+  BaseViewModel() {
 
-  //STUDY why destroy component in ViewMode.onCleared() instead of in Activity.onDestroy()
+  private val state: MediatorLiveData<State> = MediatorLiveData()
+
+  init {
+    /**
+     * Start listening to [saveQuote.liveData()]
+     *  then call [onSaveQuoteResult(SaveQuote.Result?)] whenever saveQuote.liveData().value changes
+     */
+    state.addSource(saveQuote.liveData(), ::onSaveQuoteResult)
+  }
+
+  //STUDY why destroy component in ViewModel.onCleared() instead of in Activity.onDestroy()
   override fun onCleared() {
+    saveQuote.cleanUp()
     QuotesDependencyHolder.destroyWriterComponent()
     super.onCleared()
+  }
+
+  //TODO set quote as mandatory
+  fun onClickSave(quote: String, author: String, sourceName: String, sourceUrl: String) {
+    saveQuote.execute(QuoteEntity(quote, author, sourceName, sourceUrl))
+  }
+
+  fun state(): LiveData<State> = state
+
+  private fun onSaveQuoteResult(result: SaveQuote.Result?) {
+    when (result) {
+      is SaveQuote.Result.OnSuccess -> state.value = State.SaveOk
+      is SaveQuote.Result.OnError -> state.value = State.SaveFailed
+    }
+  }
+
+  sealed class State {
+    object Loading : State()
+    object SaveOk: State()
+    object SaveFailed: State()
   }
 }
