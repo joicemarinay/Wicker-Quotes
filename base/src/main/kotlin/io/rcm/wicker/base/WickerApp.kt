2a8b845b53b6d840f3c733116a1a612f194a9aa1
Joice Marinay <joicemarinay@gmail.com>
2018-06-26 18:50:45 +0800

    Setup Timber tree

--- base/src/main/kotlin/io/rcm/wicker/base/WickerApp.kt
+++ base/src/main/kotlin/io/rcm/wicker/base/WickerApp.kt
@@ -3,6 +3,7 @@ package io.rcm.wicker.base
 import android.app.Application
 import io.rcm.wicker.base.injection.components.BaseComponent
 import io.rcm.wicker.base.injection.components.DaggerBaseComponent
+import timber.log.Timber
 
 /**
  * Created by joicemarinay on 20/04/2018.
@@ -13,10 +14,17 @@ class WickerApp: Application() {
   override fun onCreate() {
     super.onCreate()
     initDependencyInjection()
+    setupTimber()
   }
 
   private fun initDependencyInjection() {
     baseComponent = DaggerBaseComponent.builder().application(this).build()
+  }
+
+  private fun setupTimber() {
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
+    }
   }
 
   companion object {
