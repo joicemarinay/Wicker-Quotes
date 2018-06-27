14fac07718afa4427a6927ae38ae8655cd56d0d8
Joice Marinay <joicemarinay@gmail.com>
2018-06-28 05:16:06 +0800

    Add click listener for QuoteWriter's close button

--- quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterActivity.kt
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/writer/presentation/QuoteWriterActivity.kt
@@ -42,6 +42,7 @@ internal class QuoteWriterActivity(override val layoutResourceId: Int = R.layout
   }
 
   private fun setClickListeners() {
+    quoteWriter_button_close.setOnClickListener { onBackPressed() }
     quoteWriter_button_save.setOnClickListener { viewModel.onClickSave(quote = inputQuote(),
       author = inputAuthor(), sourceName = inputSource(), sourceUrl = inputLink()) }
   }
