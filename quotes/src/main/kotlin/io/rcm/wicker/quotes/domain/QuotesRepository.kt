bde638f3486f3e8d655936a50efa1a4115badcc3
Joice Marinay <joicemarinay@gmail.com>
2018-07-01 11:21:56 +0800

    Implement get quotes in data layer

--- quotes/src/main/kotlin/io/rcm/wicker/quotes/domain/QuotesRepository.kt
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/domain/QuotesRepository.kt
@@ -2,11 +2,19 @@ package io.rcm.wicker.quotes.domain
 
 import io.rcm.wicker.quotes.domain.model.QuoteEntity
 import io.reactivex.Completable
+import io.reactivex.Flowable
 
 /**
  * Created by joicemarinay on 6/24/18.
+ *
+ * Interface defining methods for how the data layer can access [QuoteEntity]s
+ * to and from the Domain layer.
+ * This is to be implemented by the data layer, setting the requirements for the
+ * operations that need to be implemented
  */
 internal interface QuotesRepository {
+
+  fun getAll(): Flowable<List<QuoteEntity>>
 
   //TODO change to Either<Failure, QuoteEntity>
   fun save(quote: QuoteEntity): Completable
