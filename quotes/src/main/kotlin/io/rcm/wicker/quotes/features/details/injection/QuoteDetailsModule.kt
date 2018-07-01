93a5de4bc538545ca2cfc93667daf03693a3691d
Joice Marinay <joicemarinay@gmail.com>
2018-07-01 23:04:58 +0800

    Add quote details in dependency graph

---
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/features/details/injection/QuoteDetailsModule.kt
@@ -0,0 +1,21 @@
+package io.rcm.wicker.quotes.features.details.injection
+
+import android.arch.lifecycle.ViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import io.rcm.wicker.base.injection.keys.ViewModelKey
+import io.rcm.wicker.quotes.features.details.presentation.QuoteDetailsViewModel
+
+/**
+ * Created by joicemarinay on 7/1/18.
+ */
+@Module
+internal abstract class QuoteDetailsModule {
+
+  @Binds
+  @IntoMap
+  @ViewModelKey(QuoteDetailsViewModel::class)
+  abstract fun bindQuoteDetailsViewModel(quoteDetailsViewModel: QuoteDetailsViewModel): ViewModel
+
+}
