4d0f39862b86fab78ca0039f5d7cb4b99d6cb333
Joice Marinay <joicemarinay@gmail.com>
2018-07-14 13:27:03 +0800

    Implement copy quote to clipboard

--- quotes/src/main/kotlin/io/rcm/wicker/quotes/features/details/presentation/QuoteDetailsViewModel.kt
+++ quotes/src/main/kotlin/io/rcm/wicker/quotes/features/details/presentation/QuoteDetailsViewModel.kt
@@ -1,21 +1,31 @@
 package io.rcm.wicker.quotes.features.details.presentation
 
 import android.arch.lifecycle.MutableLiveData
+import android.content.Context
 import io.rcm.wicker.base.presentation.BaseViewModel
 import io.rcm.wicker.quotes.QuotesDependencyHolder
 import io.rcm.wicker.quotes.presentation.QuoteUi
+import io.rcm.wicker.quotes.presentation.ResourceProvider
+import io.rcm.wicker.quotes.presentation.copyToClipBoard
 import javax.inject.Inject
 
 /**
  * Created by joicemarinay on 7/1/18.
  */
-internal class QuoteDetailsViewModel @Inject constructor(): BaseViewModel() {
+internal class QuoteDetailsViewModel @Inject constructor(
+  private val resourceProvider: ResourceProvider): BaseViewModel() {
 
-  val quote: MutableLiveData<QuoteUi> = MutableLiveData<QuoteUi>()
+  val onCopySuccess: MutableLiveData<Boolean> = MutableLiveData()
+  val quote: MutableLiveData<QuoteUi> = MutableLiveData()
 
   override fun onCleared() {
     QuotesDependencyHolder.destroyDetailsComponent()
     super.onCleared()
+  }
+
+  fun copyQuoteToClipboard(context: Context) {
+    quote.value?.copyToClipBoard(context, resourceProvider)
+    onCopySuccess.postValue(true)
   }
 
   fun setQuote(quote: QuoteUi) {
