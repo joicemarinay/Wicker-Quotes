a9caff912dfee3a8afd65ddaacb71c6514fddd54
Joice Marinay <joicemarinay@gmail.com>
2018-08-05 14:13:00 +0800

    Setup ViewModel and State of onboarding feature

---
+++ onboarding/src/main/java/io/rcm/wicker/onboarding/presentation/OnboardingViewModel.kt
@@ -0,0 +1,17 @@
+package io.rcm.wicker.onboarding.presentation
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
+import io.rcm.wicker.base.presentation.BaseViewModel
+import io.rcm.wicker.onboarding.injection.OnboardingDependencyHolder
+import javax.inject.Inject
+
+/**
+ * Created by joicemarinay on 02/08/2018.
+ */
+internal class OnboardingViewModel @Inject constructor(): BaseViewModel<OnboardingState>() {
+
+  private val uiState: MediatorLiveData<OnboardingState> = MediatorLiveData()
+
+  override fun state(): LiveData<OnboardingState> = uiState
+}
