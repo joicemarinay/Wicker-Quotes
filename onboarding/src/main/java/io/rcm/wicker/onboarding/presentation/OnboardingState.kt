5170cbe2e5a7ffe5b87d23a3d1c4b83b6328ee1b
Joice Marinay <joicemarinay@gmail.com>
2018-08-05 14:13:00 +0800

    Setup ViewModel and State of onboarding feature

---
+++ onboarding/src/main/java/io/rcm/wicker/onboarding/presentation/OnboardingState.kt
@@ -0,0 +1,8 @@
+package io.rcm.wicker.onboarding.presentation
+
+import io.rcm.wicker.base.presentation.BaseUiState
+
+/**
+ * Created by joicemarinay on 02/08/2018.
+ */
+internal sealed class OnboardingState: BaseUiState()
