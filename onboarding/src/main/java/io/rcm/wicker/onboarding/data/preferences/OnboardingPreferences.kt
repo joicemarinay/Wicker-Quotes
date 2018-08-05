ac5ac60fa5ec38b3f5bae2def67f9a58af203085
Joice Marinay <joicemarinay@gmail.com>
2018-08-06 01:18:33 +0800

    Make OnboardingPreferences internal

--- onboarding/src/main/java/io/rcm/wicker/onboarding/data/preferences/OnboardingPreferences.kt
+++ onboarding/src/main/java/io/rcm/wicker/onboarding/data/preferences/OnboardingPreferences.kt
@@ -3,7 +3,7 @@ package io.rcm.wicker.onboarding.data.preferences
 /**
  * Created by joicemarinay on 06/08/2018.
  */
-interface OnboardingPreferences {
+internal interface OnboardingPreferences {
 
   fun isOnboardingComplete(): Boolean
 
