59d673e61919c5d259c66d077f2072b73b83d7e4
Joice Marinay <joicemarinay@gmail.com>
2018-08-06 01:18:22 +0800

    Wire ShowOnboarding use case in ViewModel

--- onboarding/src/main/java/io/rcm/wicker/onboarding/presentation/OnboardingState.kt
+++ onboarding/src/main/java/io/rcm/wicker/onboarding/presentation/OnboardingState.kt
@@ -7,6 +7,7 @@ import io.rcm.wicker.base.presentation.BaseUiState
  */
 internal sealed class OnboardingState: BaseUiState() {
 
+  object ExitOnboarding: OnboardingState()
   data class PageChange(val isGetStartedVisible: Boolean): OnboardingState()
   data class PagesLoaded(val pages: List<OnboardingPage>): OnboardingState()
 }
