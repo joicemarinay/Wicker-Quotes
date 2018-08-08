70ec580029730d63dd4eb985b7708b852c54bd3f
Joice Marinay <joicemarinay@gmail.com>
2018-08-08 21:06:09 +0800

    Setup Crashlytics

--- base/src/main/kotlin/io/rcm/wicker/base/WickerApp.kt
+++ base/src/main/kotlin/io/rcm/wicker/base/WickerApp.kt
@@ -1,10 +1,13 @@
 package io.rcm.wicker.base
 
 import android.app.Application
+import android.util.Log
+import io.fabric.sdk.android.Fabric
 import io.rcm.wicker.base.injection.components.BaseComponent
 import io.rcm.wicker.base.injection.components.DaggerBaseComponent
 import io.rcm.wicker.base.injection.modules.AppModule
 import timber.log.Timber
+import com.crashlytics.android.Crashlytics
 
 /**
  * Created by joicemarinay on 20/04/2018.
@@ -15,11 +18,19 @@ class WickerApp: Application() {
   override fun onCreate() {
     super.onCreate()
     initDependencyInjection()
+    setupAnalytics()
     setupTimber()
   }
 
   private fun initDependencyInjection() {
     baseComponent = DaggerBaseComponent.builder().appModule(AppModule(this)).build()
+  }
+
+  private fun setupAnalytics() {
+    Fabric.with(Fabric.Builder(this)
+      .kits(Crashlytics())
+      .debuggable(BuildConfig.DEBUG)
+      .build())
   }
 
   private fun setupTimber() {
